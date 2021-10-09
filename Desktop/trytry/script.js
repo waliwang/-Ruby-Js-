14a35b042b9f99485fe10dac7f4d577561585964
wali <larts393@gmail.com>
2021-10-09 19:00:00 +0800

    學習ruby的運算符

---
+++ Desktop/trytry/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.querySelector("#btn")
+
+    btn.addEventListener("click", () => {
+        console.log("被按了")
+    })    
+})
